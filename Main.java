23e8dd0de75e4748caff382eaa4289db11ea8791
Mushroom Soup <EventfullGem6829@gmail.com>
2023-09-20 15:12:46 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+//let text = "How are you doing today?";
+//const myArray = text.split(" ");
+//let word = myArray[1];
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        String content = new String(Files.readAllBytes(Paths.get("text.txt")));
+        System.out.println(content);
+        List<String> textFile = new ArrayList<String>(Arrays.asList(content.split(" ")));
+        System.out.println("Array List: " + textFile);
+    }
+}
+
+
+
+
+
